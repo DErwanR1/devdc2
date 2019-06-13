8f948b7c8f6584af85244194c6b5c6ae3b027e08
Erwan <e.dubois53@gmail.com>
2019-06-13 16:45:18 +0200

    partie projet

--- php/ecolidaire/ecolidaire-master/admin/crud/category/index.php
+++ php/ecolidaire/ecolidaire-master/admin/crud/category/index.php
@@ -1,29 +1,59 @@
-<?php require_once __DIR__ . "/../../layout/header.php";
+<?php
+require_once __DIR__ . "/../../../model/database.php";
+
 $categories = getAllRows("category");
-require_once __DIR__ . "/../../layout/footer.php"; ?>
 
-<h1>Gestion des catégories</h1>
+require_once __DIR__ . "/../../layout/header.php";
+?>
 
-<a href="create-form.php" class="btn btn-primary">
-    <i class=""fa fa-plus></i>
-    Ajouter
-</a>
+    <h1>Gestion des catégories</h1>
 
-<table class="table table-striped table-bordered">
-    <thead class="thead-light">
-    <tr>
-        <th>Libellé</th>
-        <th>Actions</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($categories as $category) : ?>
-    <tr>
-        <td><?= $category["label"]; ?></td>
-        <td></td>
-    </tr>
+    <a href="create-form.php" class="btn btn-primary">
+        <i class="fa fa-plus"></i>
+        Ajouter
+    </a>
+
+<?php if (isset($_SESSION["flash"])): ?>
+    <?php foreach ($_SESSION["flash"] as $error) : ?>
+        <div class="alert alert-<?= $error["type"]; ?> alert-dismissible fade show">
+            <?= $error["message"]; ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
     <?php endforeach; ?>
-    </tbody>
-</table>
+    <?php unset($_SESSION["flash"]); ?>
+<?php endif; ?>
 
+    <hr>
 
+    <table class="table table-striped table-bordered">
+        <thead class="thead-light">
+        <tr>
+            <th>Libellé</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($categories as $category) : ?>
+            <tr>
+                <td><?= $category["label"]; ?></td>
+                <td>
+                    <a href="update-form.php"?id="<?= $category["id"]; ?>" class="btn btn-warning">
+                        <i class="fa fa-edit"></i>
+                        Modifier
+                    </a>
+                    <form method="post" action="delete-query.php">
+                        <input type="hidden" name="id" value="<?= $category["id"]; ?>">
+                        <button type="submit" class="btn btn-danger">
+                            <i class="fa fa-trash"></i>
+                            Supprimer
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+<?php require_once __DIR__ . "/../../layout/footer.php"; ?>
--- php/ecolidaire/ecolidaire-master/admin/layout/menu.php
+++ php/ecolidaire/ecolidaire-master/admin/layout/menu.php
@@ -2,14 +2,18 @@
     <li class="nav-item">
         <a class="nav-link active" href="<?= SITE_ADMIN; ?>">
             <i class="fa fa-home"></i>
-            <span data-feather="home"></span>
             Dashboard <span class="sr-only">(current)</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= SITE_ADMIN; ?>crud/project/">
+            <i class="fa fa-briefcase"></i>
+            Projets
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="<?= SITE_ADMIN; ?>crud/category/">
             <i class="fa fa-tags"></i>
-            <span data-feather="file"></span>
             Catégories
         </a>
     </li>
