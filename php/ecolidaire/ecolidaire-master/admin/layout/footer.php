ec889b78293d95031b34288445905118a458aaff
Erwan <e.dubois53@gmail.com>
2019-06-14 10:48:14 +0200

    Utilisation de libraire javascript

--- php/ecolidaire/ecolidaire-master/admin/layout/footer.php
+++ php/ecolidaire/ecolidaire-master/admin/layout/footer.php
@@ -1,7 +1,10 @@
 </main>
 </div>
 </div>
-<script src="<?=SITE_ADMIN; ?>node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?= SITE_ADMIN; ?>node_modules/jquery/jquery.min.js"></script>
 <script src="<?= SITE_ADMIN; ?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?= SITE_ADMIN; ?>node_modules/select2/dist/js/select2.min.js"></script>
+<script src="<?= SITE_ADMIN; ?>node_modules/tinymce/tinymce.min.js"></script>
+<script src="<?= SITE_ADMIN; ?>js/script.js"></script>
 </body>
 </html>
